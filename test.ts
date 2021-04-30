2163ea8b1e013417ce1b4d20332cffc4cf5d0cdd
gdoyoncsc <51342153+gdoyoncsc@users.noreply.github.com>
2021-04-29 21:13:47 -0400

    Mettre à jour test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
 // les tests vont ici; ceci n'est pas compilé lorsque cet ensemble est utilisé en tant qu'extension.
+for(let i = 0; i < 25; ++i) {
+    screenMagic.plotAt(i);
+    basic.pause(500)
+}
